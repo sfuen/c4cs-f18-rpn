ca9ab38c7d447776f31d9996847b9ab0dcb6b072
Sebastian Fuentes <sfuen@umich.edu>
2018-11-06 01:33:33 -0500

    Updated rpn.py and test_rpn.py to up-to-date with lecture implementations

--- rpn.py
+++ rpn.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import operator
 
 
@@ -9,31 +10,27 @@ op = {
     '/': operator.floordiv,
 }
 
-def calculate(arg):
-    #stack for calculator
-    stack = arg.split()
-
-    #process tokens
-    while len(stack) > 1:
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
         try:
-            value = int(token)
-            stack.append(value)
+            token = int(token)
+            stack.append(token)
         except ValueError:
-            val2 = int(stack.pop())
-            val1 = int(stack.pop())
-
-            #Look up function in table
-            func = op[token]
-            result = func(val1, val2)
-
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
             stack.append(result)
-            
-    return int(stack[0])
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
-        print(result)
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
--- test_rpn.py
+++ test_rpn.py
@@ -1,23 +1,17 @@
 import unittest
+
 import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
-
-    def test_mul(self):
-        result = rpn.calculate("2 3 *") 
-        self.assertEqual(6, result)
-
     def test_sub(self):
-        result = rpn.calculator(' 4 3 -')
-        self.assertEqual(1, result)
-
-    def test_div(self):
-        result = rpn.calculate("6 3 /")
+        result = rpn.calculator("5 3 -")
         self.assertEqual(2, result)
-
-    def test_chain(self):
-        result = rpn.calculator('1 1 + 2 *')
-        self.assertEqual(4, result)
+    def test_multiply(self):
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
+    def test_divide(self):
+        result = rpn.calculator("6 3 /")
+        self.assertEqual(2, result)
