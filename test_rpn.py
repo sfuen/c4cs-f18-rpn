faa000f872e9a1a65c177c871fb8f766c391b88e
Sebastian Fuentes <sfuen@umich.edu>
2018-11-06 17:23:17 -0500

    Added support and testing for stack rotation

--- test_rpn.py
+++ test_rpn.py
@@ -28,6 +28,12 @@ class TestBasics(unittest.TestCase):
         rpn.calculate("2 3 *", stack)
         rpn.calculate("1 1", stack)
         self.assertListEqual([6, 1, 1], stack)
+    def test_rotate(self):
+        stack = list()
+        rpn.calculate("1 2 3 4", stack)
+        rpn.rotate(stack)
+        print(stack)
+        self.assertListEqual([4, 3, 2, 1], stack)
 
 if __name__ == '__main__':
     unittest.main()
